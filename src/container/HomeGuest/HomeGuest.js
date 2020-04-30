f13ec9a6cfb9faecc24a066b62c7c11d9eaa34bb
Khay <altaircarn@gmail.com>
2020-04-30 11:55:17 -0500

    [ADD] responsive first page

--- src/container/HomeGuest/HomeGuest.js
+++ src/container/HomeGuest/HomeGuest.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react';
 
-import "./HomeGuest.scss";
+import './HomeGuest.scss';
 
-import PrincipalContent from "./PrincipalContent/PrincipalContent";
-import About from "./About/About";
+import PrincipalContent from './PrincipalContent/PrincipalContent';
+import About from './About/About';
 
 const HomeGuest = () => {
-  return (
-    <article className="HomeGuest">
-      <PrincipalContent />
-      <About />
-    </article>
-  );
+	return (
+		<article className='HomeGuest'>
+			<PrincipalContent />
+			<About />
+		</article>
+	);
 };
 
 export default HomeGuest;
