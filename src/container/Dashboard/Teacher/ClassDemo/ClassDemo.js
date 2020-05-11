441df68cad1fa10310064548b6658fad6e3d8238
Khay <altaircarn@gmail.com>
2020-05-11 13:36:14 -0500

    ADD

--- src/container/Dashboard/Teacher/ClassDemo/ClassDemo.js
+++ src/container/Dashboard/Teacher/ClassDemo/ClassDemo.js
@@ -1,29 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./ClassDemo.scss";
-import DemoStudents from "./DemoStudents/DemoStudents";
+import React from 'react';
+import { Link, NavLink, Switch, Route } from 'react-router-dom';
+import './ClassDemo.scss';
+import DemoStudents from './DemoStudents/DemoStudents';
+import TeacherPortfolio from '../../../../components/TeacherPortfolio/TeacherPortfolio';
 
-const ClassDemo = () => {
-  return (
-    <section>
-      <div className="navLinks">
-        <article className="student-content">
-          <Link style={{ color: "#fff" }} to="/">
-            <h4>Classroom</h4>
-          </Link>
-          <Link style={{ color: "#fff" }} to="/">
-            <h4>Portafolio</h4>
-          </Link>
-          <Link style={{ color: "#fff" }} to="/">
-            <h4>Mensajes</h4>
-          </Link>
-        </article>
-      </div>
-      <div>
-        <DemoStudents />
-      </div>
-    </section>
-  );
+const ClassDemo = ({ match }) => {
+	return (
+		<section>
+			<nav className='navLinks'>
+				<ul className='student-content'>
+					<li>
+						<NavLink
+							activeStyle={{ color: '#333333' }}
+							to={`${match.url}/classroom`}>
+							<h4>Classroom</h4>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							activeStyle={{ color: '#333333' }}
+							to={`${match.url}/portfolio`}>
+							<h4>Portafolio</h4>
+						</NavLink>
+					</li>
+					<li>
+						<NavLink activeStyle={{ color: '#333333' }} to='/messages'>
+							<h4>Mensajes</h4>
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+			<div>
+				<Switch>
+					<Route path={`${match.url}/classroom`} component={DemoStudents} />
+					<Route path={`${match.url}/portfolio`} component={TeacherPortfolio} />
+				</Switch>
+			</div>
+		</section>
+	);
 };
 
 export default ClassDemo;
