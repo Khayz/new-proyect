ea8889df266ed1dfacef5e8dc7157d1849663634
Adrian Strozzi <adrianstrozzi@gmail.com>
2020-05-11 19:09:41 -0500

    Added responsive design to dashboard padres

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import { authUser, getChilds } from "./redux/actions/index.actions";
-import {} from "react-toastify";
+import { } from "react-toastify";
 import "./App.scss";
 
 import Navbar from "./components/Navbar/Navbar";
@@ -16,6 +16,7 @@ import LineGraph from "./chart";
 import Assignments from "./container/Assignments/Assignments";
 import Teacher from "./container/Teacher/Teacher";
 import DemoClass from "./container/Teacher/DemoClass/DemoClass";
+
 
 function App({ authUser, user, getChilds }) {
   const { _id } = user;
@@ -31,12 +32,12 @@ function App({ authUser, user, getChilds }) {
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Route exact path="/" component={Teacher} />
-        {/*        <Route
+        {/* <Route exact path="/" component={Teacher} /> */}
+        <Route
           exact
           path="/"
           render={() => (_id ? <Dashboard /> : <HomeGuest />)}
-        /> */}
+        />
         <Route
           path="/login"
           render={() => (_id ? <Redirect to="/" /> : <Login />)}
