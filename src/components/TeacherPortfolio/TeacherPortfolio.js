9639b4bb25697ddc6231095421e5cb0415a902be
Khay <altaircarn@gmail.com>
2020-05-24 19:19:57 -0500

    [AD

--- src/components/TeacherPortfolio/TeacherPortfolio.js
+++ src/components/TeacherPortfolio/TeacherPortfolio.js
@@ -112,16 +112,18 @@ const TeacherPortfolio = ({
 									<div>
 										<h5 className='card-title'>{task.title}</h5>
 
-										<button
-											className='btn-task-file m-2'
-											onClick={() => {
-												setActualTask(task);
-												setSelectedTaskFiles(task.files);
-												setOpenTaskModal(true);
-												setSelectedTaskTitle(task.title);
-											}}>
-											Calificar
-										</button>
+										{task.status === 'Pendiente' && (
+											<button
+												className='btn-task-file m-2'
+												onClick={() => {
+													setActualTask(task);
+													setSelectedTaskFiles(task.files);
+													setOpenTaskModal(true);
+													setSelectedTaskTitle(task.title);
+												}}>
+												Calificar
+											</button>
+										)}
 									</div>
 								</form>
 							</div>
